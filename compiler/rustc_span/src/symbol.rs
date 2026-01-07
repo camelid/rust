057b38bfb2a5cb901b7642e353bc53e76088bb4c
Noah Lev <camelidcamel@gmail.com>
2026-01-07 15:22:47 -0800

    Implement MVP for opaque generic const arguments
    This is meant to be the interim successor to generic const expressions.
    Essentially, const item RHS's will be allowed to do arbitrary const
    operations using generics. The limitation is that these const items will
    be treated opaquely, like ADTs in nominal typing, such that uses of them
    will only be equal if the same const item is referenced. In other words,
    two const items with the exact same RHS will not be considered equal.
    
    I also added some logic to check feature gates that depend on others
    being enabled (like oGCA depending on mGCA).

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1608,6 +1608,7 @@ symbols! {
         on_const,
         on_unimplemented,
         opaque,
+        opaque_generic_const_args,
         opaque_module_name_placeholder: "<opaque>",
         open_options_new,
         ops,
